270d1954a2604c0ee669ea106a21a6f0b8a79c0e
HoQuocKhai <HoQuocKhai@users.noreply.github.com>
2026-06-29 09:02:40 +0700

    EPIC 14 - DOCUMENT

--- src/main/java/com/re/rikkei_bank/security/SecurityConfig.java
+++ src/main/java/com/re/rikkei_bank/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
             .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/auth/**").permitAll()
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/openapi.yaml").permitAll()
                 .anyRequest().authenticated()
             );
 
